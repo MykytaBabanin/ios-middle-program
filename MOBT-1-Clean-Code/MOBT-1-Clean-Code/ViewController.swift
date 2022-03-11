ac272e514d973c0043b2e0e44d805b2c725a6f2c
Mykyta Babanin <mykyta_babanin@epam.com>
2022-03-11 19:29:00 +0200

    MOBT-1-Clean-Code

---
+++ MOBT-1-Clean-Code/MOBT-1-Clean-Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOBT-1-Clean-Code
+//
+//  Created by Mykyta Babanin on 11.03.2022.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    
+    // swiftlint:disable:next colon
+    let noWarning : String = ""
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
+
---
+++ MOBT-1-Clean-Code/Pods/Target Support Files/Pods-MOBT-1-Clean-Code/Pods-MOBT-1-Clean-Code-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MOBT_1_Clean_Code : NSObject
+@end
+@implementation PodsDummy_Pods_MOBT_1_Clean_Code
+@end
