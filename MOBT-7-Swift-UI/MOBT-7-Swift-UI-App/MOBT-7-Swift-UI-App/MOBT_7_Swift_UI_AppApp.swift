89db77a9699b3cb0e5e5358b42a820db00d8d359
Mykyta Babanin <mykyta_babanin@epam.com>
2022-07-11 21:56:19 +0300

    Test branch

---
+++ MOBT-7-Swift-UI/MOBT-7-Swift-UI-App/MOBT-7-Swift-UI-App/MOBT_7_Swift_UI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOBT_7_Swift_UI_AppApp.swift
+//  MOBT-7-Swift-UI-App
+//
+//  Created by Mykyta Babanin on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MOBT_7_Swift_UI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
